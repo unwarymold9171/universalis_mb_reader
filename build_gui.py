ddc71489b6cc650c3d108164de38ff0694228e52
Niky <unwarymold9171@gmail.com>
2022-09-05 00:08:39 -0400

    Data Center and World comboboxs auto populate

--- build_gui.py
+++ build_gui.py
@@ -17,6 +17,8 @@ class GUI(object):
         self.ui.setupUi(MainWindow)
 
         self._set_custom_action()
+        # self.default_dc_world_list()
+        self.data_center_menu_update()
         self._hide_shortcuts()
 
         MainWindow.show()
@@ -29,6 +31,9 @@ class GUI(object):
         self.ui.addButton.clicked.connect(self.add_item_by_id)
         self.ui.idList.itemClicked.connect(self.idList_selection)
         self.ui.nameList.itemClicked.connect(self.nameList_selection)
+        self.ui.regionComboBox.currentIndexChanged.connect(self.data_center_menu_update)
+        self.ui.dcComboBox.currentIndexChanged.connect(self.world_menu_update)
+        # self.ui.clearButton.clicked.connect(self.default_dc_world_list)
 
         # Hidden menubar items, because by default there can only be one shortcut assigned
         self.ui.actionAdd.triggered.connect(self.ui.addButton.click)
@@ -59,39 +64,78 @@ class GUI(object):
     def nameList_selection(self):
         self.ui.idList.clearSelection()
 
+    def data_center_menu_update(self):
+        region = self.ui.regionComboBox.currentText()
+        data_centers = dc_list(region)
+        self.ui.dcComboBox.clear()
+        self.ui.dcComboBox.addItem('All')
+        for i in range(0, len(data_centers)):
+            dc = data_centers[i][0]
+            self.ui.dcComboBox.addItem(dc)
+        self.ui.worldComboBox.addItem('-')
+
+    def world_menu_update(self):
+        region = self.ui.regionComboBox.currentText()
+        dc = self.ui.dcComboBox.currentText()
+        self.ui.worldComboBox.clear()
+        if dc == 'All':
+            self.ui.worldComboBox.addItem('-')
+            return
+
+        self.ui.worldComboBox.addItem('All')
+        world_names = world_list(dc, dc_list(region))
+
+        for world in world_names:
+            self.ui.worldComboBox.addItem(world)
+    
+    # def default_dc_world_list(self):
+    #     region = self.ui.regionComboBox.currentText()
+    #     data_centers = dc_list(region)
+    #     self.ui.dcComboBox.addItem('All')
+    #     for i in range(0, len(data_centers)):
+    #         dc = data_centers[i][0]
+    #         self.ui.dcComboBox.addItem(dc)
+    #     self.ui.worldComboBox.addItem('-')
+
     def test(self):
         # selected = self.idList.selectedIndexes()[0].row()
         # print(selected)
-        self.ui.dcComboBox.clear()
-        self.ui.dcComboBox.addItem('All')
+        # print(self.ui.regionComboBox.currentText())
+        # self.ui.dcComboBox.clear()
+        # self.ui.dcComboBox.addItem('All')
         # self.comboBox_2
         return
 
-def dc_json() -> pd.DataFrame:
+def dc_json_to_pandas() -> pd.DataFrame:
     df = pd.read_json(json.dumps(dcs))
     df = df.set_index('region')
     return df
 
-def worlds_json() -> pd.DataFrame:
+def worlds_json_to_pandas() -> pd.DataFrame:
     df = pd.read_json(json.dumps(worlds))
     df = df.set_index('id')
     return df
 
-def dc_list(data_center:str) -> list:
+def dc_list(region_center:str) -> list:
     dc_return = []
-    _dc_list_ = dc_json().to_numpy()
+    _dc_list_ = dc_json_to_pandas().reset_index().to_numpy()
 
     for dc in _dc_list_:
-        if dc[0] == data_center:
-            dc_return.append(dc)
+        if dc[0] == region_center or (dc[0] == '中国' and region_center == 'China'):
+            dc_return.append(list(dc[1:]))
 
     return dc_return
 
 def world_list(data_center:str, dc_info:list) -> list:
-    
-    return
+    world_names = worlds_json_to_pandas().T
+
+    for dc in dc_info:
+        if dc[0] == data_center:
+            return list(world_names[dc[1]].to_numpy()[0])
+
+    return None
 
 if __name__ == "__main__":
-    # ui = GUI()
+    ui = GUI()
 
-    print('asdfasdfasdf')
+    # print('asdfasdfasdf')
