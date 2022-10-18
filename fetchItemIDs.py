b2d5de7c4bc6899ec3eebbc970b0a7ce113d14a9
Niky <unwarymold9171@gmail.com>
2022-10-17 21:43:53 -0400

    Code cleanup

--- fetchItemIDs.py
+++ fetchItemIDs.py
@@ -6,22 +6,21 @@ LODESTONE_ITEM_URLID = 'https://raw.githubusercontent.com/Asvel/ffxiv-lodestone-
 
 LODESTONE_BASE_URL = 'https://na.finalfantasyxiv.com/lodestone/playguide/db/item/'
 
-__lodestone_info = pd.read_csv(CSV, sep=',', low_memory=False, header=1, skiprows=[2,3], index_col=[0])
+__lodestone_info__ = pd.read_csv(CSV, sep=',', low_memory=False, header=1, skiprows=[2,3], index_col=[0])
 
-# __lodestone_urls = pd.read_csv(LODESTONE_ITEM_URLID, low_memory=False, header=None) # Pandas skips blank lines
-__lodestone_urls = requests.get(LODESTONE_ITEM_URLID).text.splitlines()
+__lodestone_urls__ = requests.get(LODESTONE_ITEM_URLID).text.splitlines()
 
 
 def fetch_lodestone_page(itemID:int) -> str:
-    url_id = __lodestone_urls[itemID-1]
+    url_id = __lodestone_urls__[itemID-1]
     url = LODESTONE_BASE_URL + url_id
     return url
 
 def get_item_name(itemID:int):
-    if __lodestone_urls[itemID-1] == '':
+    if __lodestone_urls__[itemID-1] == '':
         return ''
 
-    item_info = __lodestone_info.T[itemID]
+    item_info = __lodestone_info__.T[itemID]
 
     return item_info['Singular']
 
@@ -29,8 +28,8 @@ def get_item_name(itemID:int):
 
 if __name__ == '__main__':
 
-    print(__lodestone_info)
-    print(__lodestone_urls)
+    print(__lodestone_info__)
+    print(__lodestone_urls__)
 
     # 37833 (Ilmenite Ingot) = 9288158e865
     print(fetch_lodestone_page(37833))
